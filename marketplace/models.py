48cdd840403cf176b70a3199aa8202e388cac6b4
filip7557 <filip.curic@student.ferit.hr>
2024-01-21 20:27:40 +0100

    Add models

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    isSeller = models.BooleanField
+
+class Ad(models.Model):
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=512)
+    pub_date = models.DateTimeField
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    isActive = models.BooleanField
+    image = models.CharField(max_length=128)
+
+class Review(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    rating = models.IntegerField
+    text = models.CharField(max_length=512)
+
+class Dispute(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    text = models.CharField(max_length=512)
+    isSolved = models.BooleanField
---
+++ marketplace/urls.py
@@ -0,0 +1,4 @@
+app_name = "marketplace"
+urlpatterns = [
+    
+]
