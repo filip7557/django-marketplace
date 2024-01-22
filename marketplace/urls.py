3f3bbe7c2f05033ddcaddb0a3231e9eb6d5d80d5
filip7557 <filip.curic@student.ferit.hr>
2024-01-22 20:42:35 +0100

    Add buy functionality

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from marketplace.views import new
+from marketplace.views import new, buy
 
 
 app_name = "marketplace"
 urlpatterns = [
-    path("new", new, name="new")
+    path("new", new, name="new"),
+    path("buy", buy, name="buy"),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import Ad
+from .models import Ad, MarketplaceUser
 
 # Create your views here.
 def home(request):
     sort = request.GET.get('filter', 'all')
     if(sort=='all'):
-        ads = Ad.objects.order_by("-pub_date")
+        ads = filter(lambda ad: ad.isActive, Ad.objects.order_by("-pub_date"))
     else:
-        ads = filter(lambda ad: ad.type == sort, Ad.objects.order_by("-pub_date"))
+        ads = filter(lambda ad: ad.type == sort and ad.isActive, Ad.objects.order_by("-pub_date"))
+    if request.user.is_authenticated:
+        user = get_object_or_404(MarketplaceUser, pk=request.user.id)
+    else:
+        user = request.user
     context = {
         'ads': ads,
+        'user': user,
     }
     return render(request, 'marketplace/home.html', context)
+
+def buy(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    if request.method=="POST":
+        id=request.POST['id']
+        ad = get_object_or_404(Ad, pk=id)
+        user = get_object_or_404(MarketplaceUser, pk=request.user.id)
+        if user.credits >= ad.price:
+            ad.isActive = False
+            ad.save()
+            user.credits -= ad.price
+            user.save()
+            #maybe add purchase model so we can save purchases
+    return HttpResponseRedirect(reverse('home'))
 
 def new(request): #treba ga napraviti
     return reverse('home')
